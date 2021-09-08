14179514669626b353fecde049553efb09faba07
NidiaGarcia1 <nidiagares@gmail.com>
2021-09-08 00:01:59 -0500

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent  implements OnInit, AfterViewInit, OnDestroy{
+  usuario: any = [];
+
+  constructor(){
+    this.usuario = {
+      nombre: 'Cesar',
+      apellido: 'Gonzalez',
+      nacionalidad: 'peruano',
+    };
+    let usuario_temp = JSON.parse(JSON.stringify(this.usuario));
+    console.log("Constructor", usuario_temp);
+  }
+
+  ngOnInit(){
+    this.usuario.nombre = 'Carlos'
+    let usuario_temp = JSON.parse(JSON.stringify(this.usuario));
+    console.log("OnInit", this.usuario);
+  }
+
+  ngAfterViewInit(){
+    this.usuario.nombre = 'Ricardo'    
+    let usuario_temp = JSON.parse(JSON.stringify(this.usuario));
+    console.log('ViewInit', this.usuario);
+  }
+
+  ngOnDestroy(){
+    console.log('ngOnDestroy');
+  }
 }
